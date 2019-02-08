3201bd2951d3a94427ea0f09533ff4768a85c7df
AleksandrRyabyshev <ssori3838@gmail.com>
2019-02-08 15:39:00 +0200

    Create project Weather_forecast (React)

---
+++ src/containers/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+//import logo from '../logo.svg';
+import '../css/App/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Ну всё пишем
+      </div>
+    );
+  }
+}
+
+export default App;
